eth-p/bat-extras

commit 5749d9ea2d6bcabec5b3f529b0df9c1382026c06

Fix lib/opt.sh not working on Bash 5 (#2)

lib/opt.sh
@@ -7,6 +7,13 @@
 # -----------------------------------------------------------------------------
 PROGRAM="$(basename "${BASH_SOURCE[0]}")"
 
+# Sets the internal _ARGV and _ARGV_INDEX variables used when
+# parsing options with the shiftopt and shiftval functions.
+setargs() {
+	_ARGV=("$@") 
+	_ARGV_INDEX="$((${#_ARGV[@]} - 1))"
+}
+
 # Gets the next option passed to the script.
 # 
 # Variables:
@@ -22,12 +29,6 @@ PROGRAM="$(basename "${BASH_SOURCE[0]}")"
 #         echo "$OPT = $OPT_VAL"
 #     done
 shiftopt() {
-	# Ensure _ARGV exists and has the program arguments.
-	if [[ -z ${_ARGV+x} ]]; then
-		_ARGV=("${BASH_ARGV[@]}") 
-		_ARGV_INDEX="$((${#_ARGV[@]} - 1))"
-	fi
-
 	# Read the top of _ARGV.
 	[[ "$_ARGV_INDEX" -lt 0 ]] && return 1
 	OPT="${_ARGV[$_ARGV_INDEX]}"
@@ -66,4 +67,8 @@ shiftval() {
 		exit 1
 	fi
 }
+
+
+# -----------------------------------------------------------------------------
+setargs "$@"
 

commit 469a6fc65f8b6846c1f562c0927b88adb4409e1f

Fix regression introduced in 5749d9e

lib/opt.sh
@@ -7,11 +7,12 @@
 # -----------------------------------------------------------------------------
 PROGRAM="$(basename "${BASH_SOURCE[0]}")"
 
-# Sets the internal _ARGV and _ARGV_INDEX variables used when
+# Sets the internal _ARGV, _ARGV_INDEX, and _ARGV_LAST variables used when
 # parsing options with the shiftopt and shiftval functions.
 setargs() {
 	_ARGV=("$@") 
-	_ARGV_INDEX="$((${#_ARGV[@]} - 1))"
+	_ARGV_LAST="$((${#_ARGV[@]} - 1))"
+	_ARGV_INDEX=0
 }
 
 # Gets the next option passed to the script.
@@ -30,7 +31,7 @@ setargs() {
 #     done
 shiftopt() {
 	# Read the top of _ARGV.
-	[[ "$_ARGV_INDEX" -lt 0 ]] && return 1
+	[[ "$_ARGV_INDEX" -gt "$_ARGV_LAST" ]] && return 1
 	OPT="${_ARGV[$_ARGV_INDEX]}"
 	unset OPT_VAL
 	
@@ -40,7 +41,7 @@ shiftopt() {
 	fi
 
 	# Pop array.
-	((_ARGV_INDEX--))
+	((_ARGV_INDEX++))
 	return 0
 }
 
@@ -59,7 +60,7 @@ shiftval() {
 	fi
 
 	OPT_VAL="${_ARGV[$_ARGV_INDEX]}"
-	((_ARGV_INDEX--))
+	((_ARGV_INDEX++))
 
 	# Error if no value is provided.
 	if [[ "$OPT_VAL" =~ -.* ]]; then