763d34d9c9c7f43248f0d1e297c0de0dca2f9b5b
Ethan P <eth-p+git@hidden.email>
2019-10-22 15:03:58 -0700

    Fix LIB variable

--- src/batgrep.sh
+++ src/batgrep.sh
@@ -5,7 +5,7 @@
 # Repository: https://github.com/eth-p/bat-extras
 # Issues:     https://github.com/eth-p/bat-extras/issues
 # -----------------------------------------------------------------------------
-LIB="$(cd "$(dirname "${BASH_SOURCE[0]}")" && pwd)/../lib"
+LIB="$(cd "$(dirname "$(readlink "${BASH_SOURCE[0]}" || echo "${BASH_SOURCE[0]}")")" && pwd)/../lib"
 BAT="bat"
 source "${LIB}/print.sh"
 source "${LIB}/pager.sh"
