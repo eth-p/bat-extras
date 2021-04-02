e16e05d1d4a3e005cdf3af6420ad631f1459adaf
Ethan P <eth-p+git@hidden.email>
2021-04-02 16:09:24 -0700

    lib: Add 'term_clear' function to clear the terminal screen

--- lib/term.sh
+++ lib/term.sh
@@ -1,9 +1,9 @@
-# -----------------------------------------------------------------------------
+# ----------------------------------------------------------------------------------------------------------------------
 # bat-extras | Copyright (C) 2020 eth-p | MIT License
 #
 # Repository: https://github.com/eth-p/bat-extras
 # Issues:     https://github.com/eth-p/bat-extras/issues
-# -----------------------------------------------------------------------------
+# ----------------------------------------------------------------------------------------------------------------------
 
 # Gets the width of the terminal.
 # This will return 80 unless stdin is attached to the terminal.
@@ -20,4 +20,9 @@ term_width() {
 		echo "80"
 	fi
 	return 0
+}
+
+# Clears the terminal using the ANSI escape sequences for erase screen and cursor absolute positioning.
+term_clear() {
+	printf "\x1B[3J\x1B[2J\x1B[H"
 }
