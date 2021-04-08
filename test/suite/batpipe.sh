eee3156e8a48bdfc7d4c81d802764bb458779fdc
Ethan P <eth-p+git@hidden.email>
2021-04-07 20:38:35 -0700

    test: Fix fish being incorrectly detected as installed

--- test/suite/batpipe.sh
+++ test/suite/batpipe.sh
@@ -13,6 +13,7 @@ test:detected_bash_shell() {
 test:detected_fish_shell() {
 	description "Test it can detect a bash shell."
 	command -v "fish" &>/dev/null || skip "Test requires fish shell."
+	fish -c 'exit 0' &>/dev/null || skip "Test requires fish shell." # This is in case it finds "fish" in shimexec dir.
 	
 	output="$(SHELL="fish" fish --login -c "$(batpipe_path)")"
 	grep '^set -x' <<< "$output" >/dev/null || fail "Detected the wrong shell for fish."
